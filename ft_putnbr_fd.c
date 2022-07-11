d018a9a03f16dc8f49ece404ea7e9cf35e847720
Junseo Lee <junlee2@c4r7s6.42seoul.kr>
2022-07-11 16:45:29 +0900

    2022/07/11 - 16:45:29 backup

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 15:45:16 by junlee2           #+#    #+#             */
+/*   Updated: 2022/07/11 16:21:47 by junlee2          ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+static void	ft_printer(int n, int fd)
+{
+	char	temp;
+
+	temp = -(n % 10) + '0';
+	if (n / 10 < 0)
+	{
+		write(fd, &temp, 1);
+		return;
+	}
+	ft_printer(n / 10, fd);
+	write(fd, &temp, 1);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (fd >= 0)
+		fd *= -1;
+	else
+		write(fd, '-', 1);
+	ft_printer(n, fd);
+}
