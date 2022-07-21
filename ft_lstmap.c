8a263c3993a6b3103b84ba846115dd147047a33c
Junseo Lee <junlee2@c1r6s6.42seoul.kr>
2022-07-21 15:51:24 +0900

    2022/07/21 - 15:51:24 backup

---
+++ ft_lstmap.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/21 15:39:08 by junlee2           #+#    #+#             */
+/*   Updated: 2022/07/21 15:51:22 by junlee2          ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*return_tl;
+	t_list	*temp;
+
+	return_tl = 0;
+	while (lst)
+	{
+		temp = ft_lstnew(f(lst -> content));
+		if (!temp)
+		{
+			ft_lstclear(&return_tl, del);
+			return (0);
+		}
+		ft_lstadd_back(&return_tl, temp);
+		lst = lst -> next;
+	}
+	return (return_tl);
+}
